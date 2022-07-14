44eedcc295ef35603a39cbb3c482a639d91e3b6c
yosoy bartsolo <yosoybartsolo@yosoys-MacBook-Air.local>
2022-07-14 14:41:57 -0600

    Dinamic cards on state page

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -3,10 +3,10 @@ import Footer from "../Footer";
 
 export default function MainLayout({ children }) {
   return (
-    <div className="bg-stone-200">
+    <div className="bg-stone-200 w-full">
       <Navbar />
 
-      <main>{children}</main>
+      <main className="w-full">{children}</main>
       <Footer />
     </div>
   );
--- pages/estados/[stateId].js
+++ pages/estados/[stateId].js
@@ -1,18 +1,50 @@
 import { useRouter } from "next/router";
 import MainLayout from "../../components/Layouts/MainLayout";
+import PostCard from "../../components/PostCard";
+import MaxAlert from "../../components/MaxAlert";
+
+const postsData = [
+  {
+    id: 1,
+    title: "Reporte Ejemplo 1",
+    text: "Quiero ser ovíparo.",
+  },
+  {
+    id: 2,
+    title: "Reporte Ejemplo 2",
+    text: "Soy el juepuchas",
+  },
+  { id: 3, title: "Reporte Ejemplo 3", text: "Soy el juepuchas 2" },
+];
 
 const StateDetailPage = () => {
   const router = useRouter();
   const { stateId } = router.query;
   return (
     <MainLayout>
-      <div>
-        <img
-          src={`/images/states/${stateId}.png`}
-          alt={`Imagen del estado ${stateId}`}
-        ></img>
+      <div className="w-full flex border-8 border-red-500 justify-center">
+        <div className="flex justify-between max-w-7xl w-full ">
+          <div>
+            <img
+              src={`/images/states/${stateId}.png`}
+              alt={`Imagen del estado ${stateId}`}
+            ></img>
+          </div>
+          <div className="pt-6 place-content-end ">
+            <MaxAlert
+              title="Alertas Máximas"
+              text="200"
+              className="pt-6 pb-6"
+            />
+            {postsData.map((post) => (
+              <PostCard key={post.id} title={post.title} text={post.text} />
+            ))}
+          </div>
+          {/* <div className="pt-6 pl-6">
+          <MaxAlert title="Alertas Máximas" text="200" />
+        </div> */}
+        </div>
       </div>
-
       <h1>Mostrar aqui la info del estado {stateId}</h1>
     </MainLayout>
   );
