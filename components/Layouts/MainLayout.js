9b253ba7680a0cd4b4145a67359cdd53764203cc
Bart Lopez <dalopez@uach.mx>
2022-08-03 14:44:07 -0600

    Ya suéltame Footer fixed

--- components/Layouts/MainLayout.js
+++ components/Layouts/MainLayout.js
@@ -3,10 +3,10 @@ import Footer from "../Footer";
 
 export default function MainLayout({ children }) {
   return (
-    <div className="bg-stone-200 h-full w-full">
+    <div className="bg-stone-200 h-full w-full 2xl:h-screen flex flex-col justify-between">
       <Navbar />
 
-      <main className="w-full">{children}</main>
+      <main className="w-full h-full ">{children}</main>
       <Footer />
     </div>
   );
