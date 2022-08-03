7fa232c7d7817288957fc95c1fa551943d45a1da
Andres Aguilar <aaguilarc@uach.mx>
2022-08-03 09:42:15 -0600

    Center name and map on stateid

--- pages/estaditos/[stateId].js
+++ pages/estaditos/[stateId].js
@@ -97,14 +97,14 @@ const StateDetailPage = () => {
         ) : (
           <div className="w-auto justify-items-center  ">
             <div className="flex flex-col justify-center md:flex-row md:justify-center md:w-full ">
-              <div className="">
+              <div className="md:pr-60 ">
                 <h1 className="text-center font-bold text-5xl my-5 text-teal-600 uppercase">
                   {stateId}
                 </h1>
                 <img
                   src={`/images/states/${stateId}.png`}
                   alt={`Imagen del estado ${stateId}`}
-                  className=" h-auto w-auto pr-0 md:h-auto md:w-fit object-cover lg:pr-60"
+                  className=" h-auto w-auto pr-0 md:h-auto md:w-fit object-cover "
                 ></img>
               </div>
               <div className=" overflow-auto h-128  pt-6 place-content-end ">
